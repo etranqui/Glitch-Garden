9c09ec5c927ce4e8095bad8e4a77616085b59a5f
etranqui <etranqui@hotmail.com>
2017-06-05 12:44:54 -0400

    lecture 164 completed
    Added Defender Button feature.

---
+++ Assets/Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    
+    public GameObject defenderPrefab;
+
+    private Button[] buttonArray;
+    static public GameObject selectedDefender;
+
+	// Use this for initialization
+	void Start () {
+        buttonArray = GameObject.FindObjectsOfType<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Capture on click event
+    private void OnMouseDown()
+    {
+        
+        foreach(Button thisButton in buttonArray)
+        {
+            thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+        selectedDefender = defenderPrefab;
+        Debug.Log(selectedDefender + "Selected");
+    }
+}
