b859e5c1f82e1b58e53a3df1e75640609f94bdf4
etranqui <etranqui@hotmail.com>
2017-06-05 13:07:22 -0400

    165 Completed
    Added auto "Projectiles" empty GO creation
    Deleted "ProjectileParent" prefab

--- Assets/4.Entities/Shooter.cs
+++ Assets/4.Entities/Shooter.cs
@@ -6,15 +6,26 @@ public class Shooter : MonoBehaviour {
 
     [SerializeField]
     private GameObject projectilePrefab;
-    [SerializeField]
     private GameObject projectileParent;
     [SerializeField]
     private GameObject projectileSpawn;
 
 
 
+    private void Start()
+    {
+        // Look for existing empty game object as projectile folder. If not, create it
+        projectileParent = GameObject.Find("Projectiles");
+        if (projectileParent == null)
+        {
+            projectileParent = new GameObject("Projectiles");
+        }
+    }
+
+
     private void FireProjectile()
     {
+        // Create new projectile, set parent empty GO and spawn location transform
         GameObject newProjectile = Instantiate(projectilePrefab) as GameObject;
         newProjectile.transform.parent = projectileParent.transform;
         newProjectile.transform.position = projectileSpawn.transform.position;    
